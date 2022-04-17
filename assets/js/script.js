6e28f97b647876ce1825c75e4d1bc9e28b7de082
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-17 14:38:55 -0600

    added new button classes, hover states for draggable tasks and trash at bottom only appears when needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,16 +153,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event){
-    // console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event){
-   // console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event){
-   // console.log("over", event.target);
+   $(event.target).addClass("dropover-active");
   },
   out: function(event){
-   // console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event){
     var tempArr = [];
@@ -211,7 +213,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -245,14 +247,14 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui){
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
     ui.draggable.remove();
   },
   over: function(event, ui){
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -277,6 +279,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 }
+
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  })
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
