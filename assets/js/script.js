6c920d44b8ba6757a8db3dfeeb2b756b251e590b
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-16 19:19:34 -0600

    Added edit functionality on click to the date spans and the p elements for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ var saveTasks = function () {
 };
 
 $(".list-group").on("click", "p", function () {
-  var text = $(this).text();
+  var text = $(this).text().trim();
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
@@ -82,6 +82,58 @@ $(".list-group").on("blur", "textarea", function () {
   // replace textarea with p element
   $(this).replaceWith(taskP);
 });
+
+// due date was clicked
+
+$(".list-group").on("click", "span", function(){
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  //create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  //swap out elements
+  $(this).replaceWith(dateInput);
+
+  //automatically focus on new element
+  dateInput.trigger("focus");
+})
+
+$(".list-group").on("blur", "input[type='text']", function(){
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get parent ul's id attribute
+
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  //get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  //update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  //replace input with the span element
+  $(this).replaceWith(taskSpan);
+})
 
 
 
